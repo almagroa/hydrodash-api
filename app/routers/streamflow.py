e11dd3617d5bcac0dbafd008b445ba33f9034db2
Andre <andre.almagro@gmail.com>
2025-07-08 17:16:33 -0400

    Adiciona informações para o formato de inserção de datas

--- app/routers/streamflow.py
+++ app/routers/streamflow.py
@@ -1,18 +1,19 @@
 from fastapi import APIRouter
+from fastapi import Query
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from app.crud import fetch_streamflow
 
 router = APIRouter()
 
-@router.get("/streamflow")
+@router.get("/streamflow", tags=["Vazão"], summary="Consulta dados de vazão prevista")
 def get_streamflow(
-    bacia_id: int = None,
-    start_date: str = None,
-    end_date: str = None,
-    rodada: str = None,
-    produto_id: int = None,
-    limit: int = 100
+    bacia_id: int = Query(None, description="ID da bacia hidrográfica"),
+    start_date: str = Query(None, description="Data inicial da previsão (YYYY-MM-DD)"),
+    end_date: str = Query(None, description="Data final da previsão (YYYY-MM-DD)"),
+    rodada: str = Query(None, description="Data da rodada no formato (YYYY-MM-DD)"),
+    produto_id: int = Query(None, description="ID do modelo climático"),
+    limit: int = Query(100, description="Limite de registros retornados")
 ):
     result = fetch_streamflow(bacia_id, start_date, end_date, rodada, produto_id, limit)
     safe_result = jsonable_encoder(result)
