0f2b8798e009a343497e3b3463b2a23c074920b4
Andre <andre.almagro@gmail.com>
2025-07-07 23:59:50 -0400

    Atualiza database.py para usar vars de ambiente

--- app/database.py
+++ app/database.py
@@ -1,10 +1,11 @@
+import os
 import psycopg2
 
 def get_conn():
     return psycopg2.connect(
-        host="yamanote.proxy.rlwy.net",
-        database="railway",
-        user="postgres",
-        password="IEttkORGWOiHKfQrXdRSYqqkCUzxDFyn",
-        port="47712"
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        port=os.getenv("DB_PORT")
     )
