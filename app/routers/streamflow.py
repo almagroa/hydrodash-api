3438804bc997d3f0fef8e21efaab2b4703b495cd
Andre <andre.almagro@gmail.com>
2025-07-08 00:20:26 -0400

    alteracao no streamflow.py

--- app/routers/streamflow.py
+++ app/routers/streamflow.py
@@ -6,8 +6,12 @@ from app.crud import fetch_streamflow
 router = APIRouter()
 
 @router.get("/streamflow")
-def get_streamflow(bacia_id: int = None, start_date: str = None, end_date: str = None, limit: int = 100):
+def get_streamflow(
+    bacia_id: int = None,
+    start_date: str = None,
+    end_date: str = None,
+    limit: int = 100
+):
     result = fetch_streamflow(bacia_id, start_date, end_date, limit)
-    # Usa encoder para evitar erros com NaN, datetime etc.
     safe_result = jsonable_encoder(result)
     return JSONResponse(content=safe_result)
