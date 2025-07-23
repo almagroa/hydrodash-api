7c19c56358f7e0601c0306928d841f55497723fa
Andre <andre.almagro@gmail.com>
2025-07-22 20:52:58 -0400

    Insere a verificacao de seguranca via token v4

--- app/routers/bacias.py
+++ app/routers/bacias.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter, Query
 from app.crud import fetch_bacias
+from fastapi import Depends
+from app.auth import verify_token
 
 router = APIRouter()
 
 @router.get("/bacias", tags=["Metadados"], summary="Consulta informações sobre as bacias")
-def get_bacias(bacia_id: int = Query(None, description="ID da bacia (opcional)")):
+def get_bacias(bacia_id: int = Query(None, description="ID da bacia (opcional)"),
+               token: str = Depends(verify_token),):
     return fetch_bacias(bacia_id=bacia_id)
--- app/routers/produtos.py
+++ app/routers/produtos.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter, Query
 from app.crud import fetch_produtos
+from fastapi import Depends
+from app.auth import verify_token
 
 router = APIRouter()
 
 @router.get("/produtos", tags=["Metadados"], summary="Consulta informações sobre os modelos climáticos")
-def get_produtos(produto_id: int = Query(None, description="ID do modelo (opcional)")):
+def get_produtos(produto_id: int = Query(None, description="ID do modelo (opcional)"),
+                 token: str = Depends(verify_token),
+                 ):
     return fetch_produtos(produto_id=produto_id)
