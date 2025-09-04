e00488afaf37594648340e1a5c2c86b569e9f110
Andre <andre.almagro@gmail.com>
2025-09-04 10:40:31 -0400

    Adiciona hash de senha com bcrypt v2

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,14 +3,15 @@
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from datetime import timedelta
-from app.auth import create_access_token, fake_users_db, ACCESS_TOKEN_EXPIRE_MINUTES
+from app.auth import create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
+from app.auth import fake_users_db, authenticate_user  # <- usamos authenticate_user
 
 router = APIRouter()
 
 @router.post("/token")
 def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = fake_users_db.get(form_data.username)
-    if not user or user["password"] != form_data.password:
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
         raise HTTPException(status_code=400, detail="Usuário ou senha inválidos")
     
     token = create_access_token(
