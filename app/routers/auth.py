7052c868661c22a19fdb1fc4640b046946d177fd
Andre <andre.almagro@gmail.com>
2025-09-04 11:00:03 -0400

    Adiciona users

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,15 +3,14 @@
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from datetime import timedelta
-from app.auth import create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
-from app.auth import fake_users_db, authenticate_user  # <- usamos authenticate_user
+from app.auth import create_access_token, fake_users_db, ACCESS_TOKEN_EXPIRE_MINUTES
 
 router = APIRouter()
 
 @router.post("/token")
 def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(form_data.username, form_data.password)
-    if not user:
+    user = fake_users_db.get(form_data.username)
+    if not user or user["password"] != form_data.password:
         raise HTTPException(status_code=400, detail="Usuário ou senha inválidos")
     
     token = create_access_token(
